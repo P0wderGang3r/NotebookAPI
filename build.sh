b4e4d58958a83843d677c8e75ded3c52368e017e
P0wderGang3r <skopetskiy.ts@gmail.com>
2022-01-25 18:23:11 +1000

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sudo apt-get upgrade
 sudo apt install nginx
 sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 pip3 install virtualenv
-python3 -m virtualenv NotebookAPI
+python3 -m virtualenv NotebookAPIEnv
 source NotebookAPIEnv/bin/activate
 pip install wheel uwsgi flask peewee flask-peewee
 deactivate
