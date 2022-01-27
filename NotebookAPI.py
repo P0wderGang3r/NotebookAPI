9408a61ec8376c3da2b57b94c6e3f0164b0ea7dc
P0wderGang3r <skopetskiy.ts@gmail.com>
2022-01-28 00:22:32 +1000

    soft final #9.4

--- NotebookAPI.py
+++ NotebookAPI.py
@@ -33,7 +33,7 @@ def add_user():
 	if (len(user_query) == 0):
 		curr_id = 1
 	else:
-		curr_id = users.select().dicts().execute().curr_id[len(todo_query)].user_id + 2
+		curr_id = users.select().dicts().execute()[len(todo_query)].user_id + 2
 	return str(curr_id)
 	#curr_id = len(todo_query) + ord(curr_name[0]) + ord(curr_password[0]) + int(curr_datetime)
 	curr_session_id = str(base64.b64encode(bytes('' + curr_name + curr_password + str(curr_datetime), 'utf-8')))
